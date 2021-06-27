94078f1732302d2d12ebe047670de5c125468f54
AtulManjhi <atulmanjhi05@gmail.com>
2021-06-27 09:47:27 +0530

    make it better

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -137,9 +137,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'myrunapp005@gmail.com'
 EMAIL_HOST_PASSWORD = 'India@Cric123'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
