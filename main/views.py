5c6c95dc791dfffa2c55189a1f11262f21a6dbb9
AtulManjhi <atulmanjhi05@gmail.com>
2021-06-27 16:07:09 +0530

    Lets make it better

--- main/views.py
+++ main/views.py
@@ -35,7 +35,13 @@ def index(request):
 
         Message: {}
         '''.format(senders_name,senders_name,senders_email,subject,message)
-        
+        send_mail("New message from Portfolio.",
+        message_to_be_sent,
+        settings.EMAIL_HOST_USER,
+        ['atulmanjhi05@gmail.com'],
+        fail_silently=True,
+        auth_user=settings.EMAIL_HOST_USER,
+        auth_password='India@Cric123')
         return render(request,'main/thanks.html', context)
 
     return render(request, 'main/index.html', context)
